3d89d4a26780919a56a903ba2177312e2d9db1e3
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-26 10:11:21 -0400

    Style fixes
    Used Rubocop on the two files to fix styling issues to match with Ruby conventions.

--- lib/color.rb
+++ lib/color.rb
@@ -1,7 +1,6 @@
+# Holds a single color
 class Color
-  
   def initialize(color_id)
-    @color_id=color_id
+    @color_id = color_id
   end
-
 end
--- lib/color_options.rb
+++ lib/color_options.rb
@@ -1,17 +1,17 @@
+# Contains options for the 6 possible colors in-game. Color 0 is reset color
 module ColorOptions
-  RESET="\e[39;49m"
-  RED="\e[41m"
-  GREEN="\e[42m"
-  BLUE="\e[44m"
-  YELLOW="\e[43m"
-  WHITE="\e[30;107m"
-  BLACK="\e[97;40m"
+  RESET = "\e[39;49m"
+  RED = "\e[41m"
+  GREEN = "\e[42m"
+  BLUE = "\e[44m"
+  YELLOW = "\e[43m"
+  WHITE = "\e[30;107m"
+  BLACK = "\e[97;40m"
 
-  COLORS=[RESET, RED, GREEN, BLUE, YELLOW, WHITE, BLACK]
-  
+  COLORS = [RESET, RED, GREEN, BLUE, YELLOW, WHITE, BLACK]
+
   # gets the color by an id. When id=0 then it is a reset color
   def self.get_color_by_color_id(id)
-    return COLORS[id]
+    COLORS[id]
   end
-  
 end
